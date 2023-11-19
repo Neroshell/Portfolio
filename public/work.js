fd5deba28f22e62a035eed5ee98ff805b720eda4
Udezi Oghenero <udezioghenero@mail.com>
2023-11-19 17:21:37 +0100

    improve menu button

--- public/work.js
+++ public/work.js
@@ -6,13 +6,22 @@ AOS.init({
 
 const menuButton = document.querySelector(".mobile-icon-div");
 const stuck = document.querySelector(".stuck-nav-div-mobile");
+const body =   document.getElementsByTagName('body')[0];
+originalColor = menuButton.style.backgroundColor;
 
 
 menuButton.addEventListener("click", function(){
-    if (stuck.style.display === "block") {
-       stuck.style.display = "none";
-    } else {
-       stuck.style.display = "block";
-    }
- });
+
+   if (stuck.style.display === "block") {
+      stuck.style.display = "none";
+      body.style.position = "static";
+      this.style.backgroundColor = originalColor;  
+     
+   } else {
+      stuck.style.display = "block";
+      body.style.position = "fixed";
+      this.style.backgroundColor = "#279eff";
+      
+   }
+});
   

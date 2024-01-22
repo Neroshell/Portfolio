0c5cf0dd1e53b4515a95091f74c5f6532341f428
Udezi Oghenero <udezioghenero@mail.com>
2024-01-22 21:30:48 +0100

    rearrange stack

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const _ = require('lodash');
 const enforce = require('express-sslify');
 const port = process.env.PORT || 3000
 
- app.use(enforce.HTTPS({trustProtoHeader: true}));
+app.use(enforce.HTTPS({trustProtoHeader: true}));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended : true }));
 
